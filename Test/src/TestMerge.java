082ecf79a24b18c65368c899ef1aa9fa3107e77d
anilappana999 <anilappana999@gmail.com>
2022-06-25 20:15:47 +0530

    hjkl

--- Test/src/TestMerge.java
+++ Test/src/TestMerge.java
@@ -4,9 +4,9 @@ public class TestMerge {
 	public static void main(String[] args) {
 		System.out.println("anil");
 		
-		long we = 123;
-		long as = 4;
-		System.out.println(we+as);
+		long data = 123;
+		long data1 = 4;
+		System.out.println(data+data1);
 		
 
 	}
