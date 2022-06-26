8cb8b1740a0cd2d641aca532ecbd05ae1032da12
anilappana999 <anilappana999@gmail.com>
2022-06-26 15:29:03 +0530

    commit

--- Test/src/TestMerge.java
+++ Test/src/TestMerge.java
@@ -3,9 +3,9 @@ public class TestMerge {
 	public static void main(String[] args) {
 		System.out.println("anil");
 		
-		long s121 = 123;
-		long e31 = 4;
-		System.out.println(s121+e31);
+		long a = 123;
+		long b = 4;
+		System.out.println(a+b);
 
 
 		
