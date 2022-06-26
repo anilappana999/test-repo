ecc4e9633f98d5e3c0d51b14b4943893f9f9874e
anilappana999 <anilappana999@gmail.com>
2022-06-26 15:32:34 +0530

    fdji

--- Test/src/TestMerge.java
+++ Test/src/TestMerge.java
@@ -3,9 +3,9 @@ public class TestMerge {
 	public static void main(String[] args) {
 		System.out.println("anil");
 		
-		long s21 = 123;
-		long e314 = 4;
-		System.out.println(s21+e314);
+		long x = 123;
+		long y = 4;
+		System.out.println(x+y);
 
 
 		
