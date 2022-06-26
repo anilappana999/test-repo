1cae8ea88312d34b15369946d27441e1fd4887db
anilappana999 <39027009+anilappana999@users.noreply.github.com>
2022-06-26 15:32:14 +0530

    Update TestMerge.java

--- Test/src/TestMerge.java
+++ Test/src/TestMerge.java
@@ -3,9 +3,9 @@ public class TestMerge {
 	public static void main(String[] args) {
 		System.out.println("anil");
 		
-		long s21 = 123;
-		long e314 = 4;
-		System.out.println(s21+e314);
+		long a = 123;
+		long b = 4;
+		System.out.println(a+b);
 
 
 		
